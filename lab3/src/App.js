bf54133194d70e80a5cbcdcc341cdb2d8cf22765
leksahk <oleksandra.savchuk.oi.2024@lpnu.ua>
2026-03-20 12:50:34 +0200

    edited react

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Timer from './Timer';
 import HackathonCard from './HackathonCard';
 import ParticipantList from './ParticipantList';
